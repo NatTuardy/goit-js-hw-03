3204e9e0d5106995ac4d2c04e609d04220d26755
Your Name <you@example.com>
2020-03-12 15:02:53 +0200

    added new folder

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Напиши функцию countProps(obj), считающую
+//  кол-во свойств в объекте. Функция возвращает
+//  число - количество свойств.
+
+const countProps = function(obj) {
+  return Object.keys(obj).length;
+  // return Object.values(obj).lenght;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
